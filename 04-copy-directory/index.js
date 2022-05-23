669313bc760cd6448f7683faefb2cb8381448a79
Ilya Bondarev <ilushkabond@gmail.com>
2022-05-24 01:04:43 +0200

    feat: solve task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirFromCopy = path.resolve(__dirname, 'files');
+const dirToCopy = path.join(__dirname, 'files-copy');
+
+const copyDir = async () => {
+  fs.mkdir(dirToCopy, { recursive: true }, mkdirError => {
+    if (mkdirError) {
+      console.log(mkdirError);
+    }
+  });
+
+  const files = await fs.promises.readdir(dirFromCopy, { withFileTypes: true });
+  files.forEach(file => {
+    const fileName = file.name;
+    fs.copyFile(path.join(dirFromCopy, fileName), path.join(dirToCopy, fileName), copyError => {
+      if (copyError) {
+        console.log(copyError);
+      }
+    })
+  });
+};
+
+(() => {
+  fs.promises.rm(dirToCopy, { recursive: true, force: true }, rmError => {
+    if (rmError) {
+      console.log(rmError);
+    }
+  }).then(() => {
+    copyDir();
+  })
+})();
